5e7da03a501b2b65b960cff20d09f1b60b37bab5
Aleksandr <algryshko@gmail.com>
2017-02-03 18:34:44 +0200

    wp theme change slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,33 @@
 jQuery(document).ready(function ($){
-    $('.bxslider').bxSlider({
+    var slider = $('.bxslider').bxSlider({
         pagerCustom: '#bx-pager',
         auto: false,
         pause: 6000,
+        onSlideBefore:function($slideElement, oldIndex, newIndex){
+            changeRealThumb(pager, newIndex);
 
-
-
-
-        mode: 'horizontal',
-        slideSelector: '',
-        infiniteLoop: true,
-        hideControlOnEnd: false,
-        speed: 500,
-        easing: null,
-        slideMargin: 0,
-        startSlide: 0,
-        randomStart: false,
-        captions: false,
-        ticker: false,
-        tickerHover: false,
-        adaptiveHeight: false,
-        adaptiveHeightSpeed: 500,
-        video: false,
-        useCSS: true,
-        preloadImages: 'visible',
-        responsive: true,
-        slideZIndex: 50,
-        wrapperClass: 'bx-wrapper',
-
-        // TOUCH
-        touchEnabled: true,
-        swipeThreshold: 50,
-        oneToOneTouch: true,
-        preventDefaultSwipeX: true,
-        preventDefaultSwipeY: false,
-
-        // PAGER
-        pager: true,
-        pagerType: 'full',
-        pagerShortSeparator: ' / ',
-        pagerSelector: null,
-
-        // CONTROLS
-        controls: true,
-        nextText: 'Next',
-        prevText: 'Prev',
-        nextSelector: null,
-        prevSelector: null,
-        autoControls: false,
-        startText: 'Start',
-        stopText: 'Stop',
-        autoControlsCombine: false,
-        autoControlsSelector: null,
-
-        // AUTO
-        autoStart: true,
-        autoDirection: 'next',
-        autoHover: false,
-        autoDelay: 0,
-        autoSlideForOnePage: false,
-
-        // CAROUSEL
-        minSlides: 1,
-        maxSlides: 1,
-        moveSlides: 0,
-        slideWidth: 0,
-
-        // CALLBACKS
-        onSliderLoad: function() {},
-        onSlideBefore: function() {},
-        onSlideAfter: function() {},
-        onSlideNext: function() {},
-        onSlidePrev: function() {},
-        onSliderResize: function() {}
+        }
     });
+
+    var pager = $('#bx-pager').bxSlider({
+        slideWidth: 100,
+        minSlides: 1,
+        maxSlides: 5,
+        moveSlides: 1,
+        slideMargin: 28,
+        pager: false,
+        wrapperClass: 'page-wrapper',
+        controls: true,
+        infiniteLoop: false,
+    });
+    function changeRealThumb(slider,newIndex){
+
+        var $thumbS=$("#bx-pager");
+        $thumbS.find('.active').removeClass("active");
+        $thumbS.find('a[slideIndex="' + newIndex + '"]').addClass("active");
+
+        if( slider.getSlideCount() - newIndex >= 4 ) slider.goToSlide(newIndex);
+        else slider.goToSlide(slider.getSlideCount()-4);
+
+    }
 });
